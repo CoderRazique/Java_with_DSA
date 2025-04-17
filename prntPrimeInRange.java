6bd4cf23b9231129e5bf8fb14777ac3b07502ff0
CoderRazique <abdrazique22@gmail.com>
2025-04-17 07:21:10 +0530

    Add java file to print all prime in range

---
+++ prntPrimeInRange.java
@@ -0,0 +1,33 @@
+// Print all Prime in range
+
+import java.util.Scanner;
+
+public class prntPrimeInRange {
+    public static boolean isPrime(int n) {
+        boolean isPrime = true;
+
+        if (n == 2) {
+            isPrime = true;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                }
+            }
+        }
+        return isPrime;
+    }
+
+    public static void printPrime(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printPrime(20);
+    }
+}
