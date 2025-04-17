71be5729d554d5e546e82b24a3f8b5c7e86c5e77
CoderRazique <abdrazique22@gmail.com>
2025-04-17 08:02:27 +0530

    Upload file to conver binary to decimal

---
+++ BasicsLogicBuildProgram/binaryToDecimal.java
@@ -0,0 +1,25 @@
+// Convert from Binary to Decimal
+
+import java.util.Scanner;
+
+public class binaryToDecimal {
+    public static void binToDec(int binNum) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+        while (binNum > 0) {
+            int ld = binNum % 10;
+            decNum = decNum + (ld * (int) Math.pow(2, pow));
+            pow++;
+            binNum /= 10;
+        }
+        System.out.println("decimal of " + myNum + " = " + decNum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter binary number(01) : ");
+        int binNum = sc.nextInt();
+        binToDec(binNum);
+    }
+}
