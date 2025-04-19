893543a41533270129f2cb97eb723afeec4d217a
CoderRazique <abdrazique22@gmail.com>
2025-04-19 23:42:23 +0530

    Add code of print subarray

---
+++ Searching/printSubArray.java
@@ -0,0 +1,23 @@
+// Print subarray (a continuous part of array)
+
+public class printSubArray {
+
+    public static void pairsIn_Array(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                System.out.print("|");
+                for (int k = i; k <= j; k++) {
+                    System.out.print(" " + nums[k] + " ");
+                }
+                System.out.print("| ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 4, 6, 8, 10 };
+        pairsIn_Array(nums);
+    }
+
+}
