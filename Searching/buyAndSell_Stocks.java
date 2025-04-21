044c267d69330a19c0caaa08815dcdc6bda41cd3
CoderRazique <abdrazique22@gmail.com>
2025-04-21 07:17:39 +0530

    Buy and Sell a stock and find maximum profit

---
+++ Searching/buyAndSell_Stocks.java
@@ -0,0 +1,23 @@
+// Buy and Sell the stock
+
+public class buyAndSell_Stocks {
+    public static int buyAnsSellStock(int[] prices) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println("Max sell price of stock : " + buyAnsSellStock(prices));
+
+    }
+}
