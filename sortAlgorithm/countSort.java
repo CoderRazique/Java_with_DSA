207eefdce6c0752366552c61ac6027e235c65d9f
CoderRazique <abdrazique22@gmail.com>
2025-04-24 13:54:21 +0530

    uypload file

---
+++ sortAlgorithm/countSort.java
@@ -0,0 +1,36 @@
+// Count sort
+
+public class countSort {
+    public static void countsSort(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+
+        int[] count = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArray(int[] var0) {
+        for (int var1 = 0; var1 < var0.length; ++var1) {
+            System.out.print(var0[var1] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 1, 3, 2, 4, 3, 7 };
+        countsSort(arr);
+        printArray(arr);
+    }
+}
