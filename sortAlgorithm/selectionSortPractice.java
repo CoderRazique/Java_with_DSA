aacb03e64b71edef62721c8c701c5fc30c3cf111
CoderRazique <abdrazique22@gmail.com>
2025-04-24 17:05:44 +0530

    upload file

--- sortAlgorithm/selectionSortPractice.java
+++ sortAlgorithm/selectionSortPractice.java
@@ -1,17 +1,13 @@
 public class selectionSortPractice {
     public static void selectionSort(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            int minPos = i;
+
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] > nums[j]) {
-                    // minPos = j;
                     int temp = nums[j];
                     nums[j] = nums[i];
                     nums[i] = temp;
                 }
-                // int temp = nums[minPos];
-                // nums[minPos] = nums[i];
-                // nums[i] = temp;
             }
         }
     }
