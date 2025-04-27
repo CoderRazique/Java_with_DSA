6857f9e9fd3ca370a1cc1d6e13c2eadee6d34f04
CoderRazique <abdrazique22@gmail.com>
2025-04-27 12:21:17 +0530

    upload file

---
+++ String/stringCompression.java
@@ -0,0 +1,26 @@
+//  String compression
+//  Input : "aaabbcccdd"
+// Output : "a3b2c3d2"
+
+public class stringCompression {
+    public static String strCompress(String str) {
+        String newStr = "";
+        for (int i = 0; i < str.length(); i++) {
+            newStr += str.charAt(i);
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+
+    public static void main(String[] args) {
+        String str = "aaabcccdd";
+        System.out.println("Compress string : " + strCompress(str));
+    }
+}
