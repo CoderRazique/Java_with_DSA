2913c51ee4521e1d3ea91cd03993a5fd06e6e0a2
CoderRazique <abdrazique22@gmail.com>
2025-04-29 08:02:58 +0530

    upload file

---
+++ BitManipulation/countSetBits.java
@@ -0,0 +1,18 @@
+// Count Set Bits in a Number
+
+public class countSetBits {
+    public static int countSetBitsnNum(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Total set bits : " + countSetBitsnNum(15));
+    }
+}
