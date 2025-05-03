25f982b3a83183b097b79d94aa0a62d33b8f2885
CoderRazique <abdrazique22@gmail.com>
2025-05-03 13:43:47 +0530

    upload file

---
+++ Recursion/PrintXTTPNOptimized.java
@@ -0,0 +1,23 @@
+//  X^n  with TC = O(log2n) in optimized
+
+public class PrintXTTPNOptimized {
+
+    public static int pow(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPowerSqrt = pow(x, n / 2) * pow(x, n / 2);
+
+        if (n % 2 != 0) {
+            halfPowerSqrt = x * halfPowerSqrt;
+        }
+        return halfPowerSqrt;
+    }
+
+    public static void main(String[] args) {
+        int x = 25;
+        int n = 2;
+        System.out.println(pow(x, n));
+    }
+}
