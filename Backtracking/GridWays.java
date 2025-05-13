5c401f793380ab9a3f56bb3a549ae303be656437
CoderRazique <abdrazique22@gmail.com>
2025-05-13 12:26:09 +0530

    Grid ways solution

---
+++ Backtracking/GridWays.java
@@ -0,0 +1,22 @@
+// Grid ways
+// Find number of ways to reach from (0,0) to (n-1, m-1) in a N*M Grid.
+// Allowed moves - right or down
+
+public class GridWays {
+    public static int gridWays(int i, int j, int n, int m) {
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == n) {
+            return 0;
+        }
+
+        int w1 = gridWays(i + 1, j, n, m);
+        int w2 = gridWays(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println("Total number of ways to reach from (0,0) to (n-1, m-1) :  " + gridWays(0, 0, n, m));
+    }
+}
