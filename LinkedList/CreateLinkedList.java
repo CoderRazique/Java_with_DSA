0550f4c07ecee9769d21643053f71852dc54964b
CoderRazique <abdrazique22@gmail.com>
2025-05-21 08:37:12 +0530

    Remove node from first and last Update code

--- LinkedList/CreateLinkedList.java
+++ LinkedList/CreateLinkedList.java
@@ -15,12 +15,14 @@ public class CreateLinkedList {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     // Add node from first
     // TC = O(1)
     public void addFirst(int data) {
         // Step1 - create new node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -36,6 +38,7 @@ public class CreateLinkedList {
     // TC = O(1)
     public void addLast(int data) {
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -63,12 +66,13 @@ public class CreateLinkedList {
     // Add in the Middle
     // TC = O(n)
     public void addMiddle(int data, int idx) {
-        Node newNode = new Node(data);
-        if (head == null) {
+        if (idx == 0) {
             System.out.println("LL is empty! So there first node = tail node = middle node...");
             addFirst(data);
             return;
         }
+        Node newNode = new Node(data);
+        size++;
         Node temp = head;
         int c = 0;
         while (c < idx - 1) {
@@ -79,6 +83,48 @@ public class CreateLinkedList {
         temp.next = newNode;
     }
 
+    // Remove first node
+    // TC = O(1)
+    public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LL is empty.");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
+    // remove last node
+    // TC = O(n)
+    public int removelast() {
+        if (size == 0) {
+            System.out.println("LL is empty!");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+        Node prev = head;
+        // priv : i = size - 2;
+        for (int i = 0; i < size - 2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
     public static void main(String[] args) {
         CreateLinkedList ll = new CreateLinkedList();
         ll.addFirst(2);
@@ -87,5 +133,10 @@ public class CreateLinkedList {
         ll.addLast(4);
         ll.addMiddle(9, 2);
         ll.printLinkedList();
+        System.out.println("Remove element from first : " + ll.removeFirst());
+        ll.printLinkedList();
+        System.out.println("Remove element from last : " + ll.removelast());
+        ll.printLinkedList();
+        System.out.println("Size of linkedList : " + size);
     }
 }
