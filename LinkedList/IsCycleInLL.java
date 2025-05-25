143f38e9a81bb720af426a70b40798a66d695d9d
CoderRazique <abdrazique22@gmail.com>
2025-05-25 11:42:09 +0530

    isCycle LL solution

---
+++ LinkedList/IsCycleInLL.java
@@ -0,0 +1,50 @@
+// Cheack LL is a  loop or cycle  Linked List
+
+public class IsCycleInLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    public void printLL() {
+        if (head == null) {
+            System.out.println("LL is empty ! ");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static boolean isCycleLL() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true; // cycle exists
+            }
+        }
+        return false; // cycle does not exist
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+        System.out.println(isCycleLL());
+
+    }
+}
