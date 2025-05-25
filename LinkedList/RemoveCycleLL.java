32ebb2734df500f6c8387bff81d1ab67d02613c1
CoderRazique <abdrazique22@gmail.com>
2025-05-25 13:18:22 +0530

    removeCycle from LL solution

---
+++ LinkedList/RemoveCycleLL.java
@@ -0,0 +1,74 @@
+
+// Cheack LL is a  loop or cycle  Linked List
+
+public class RemoveCycleLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    public static void removeCycle() {
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true; // cycle exists
+                break;
+            }
+        }
+        if (cycle == false) {
+            System.out.println("LL is not cycle!");
+            return;
+        } // cycle does not exist
+
+        // find meeting points
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // remove cycle -> last.next = null
+        prev.next = null;
+    }
+
+    // isCycle
+    public static boolean isCycleLL() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true; // cycle exists
+            }
+        }
+        return false; // cycle does not exist
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        // 1->2->3->2
+        System.out.println(isCycleLL());
+        removeCycle();
+        System.out.println(isCycleLL());
+
+    }
+}
