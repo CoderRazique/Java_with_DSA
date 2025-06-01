17e0f0fd982d3e9b0b5e8a3dc9807d7fc2bbf9f6
CoderRazique <abdrazique22@gmail.com>
2025-06-01 08:04:33 +0530

    Doubly LL operation

---
+++ LinkedList/DoublyLL.java
@@ -0,0 +1,102 @@
+//  Doubly Linked List basics operations
+
+public class DoublyLL {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("DLL is empty!");
+            return -1;
+        }
+        if (head.next == null && tail.prev == null) {
+            int data = head.data;
+            head = tail = null;
+            return data;
+        }
+        int data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    public int removeLast() {
+        if (head == null) {
+            System.out.println("DLL is empty!");
+            return -1;
+        }
+        if (head.next == null && tail.prev == null) {
+            int data = head.data;
+            head = tail = null;
+            return data;
+        }
+        int data = tail.data;
+        tail = tail.prev;
+        tail.next = null;
+        return data;
+
+    }
+
+    public void printDLL() {
+        if (head == null && tail == null) {
+            System.out.println("Doubly linked list is empty!");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLL dll = new DoublyLL();
+        dll.addFirst(1);
+        dll.addFirst(2);
+        dll.addFirst(3);
+        dll.addFirst(4);
+        dll.addLast(0);
+        dll.printDLL();
+        System.out.println(dll.removeFirst() + " is successfully remove from first.");
+        dll.printDLL();
+        System.out.println(dll.removeLast() + " is successfully remove from last.");
+        dll.printDLL();
+    }
+}
