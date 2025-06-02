a75baa3038d2176b7ce29f6cc482d22d4579fa50
CoderRazique <abdrazique22@gmail.com>
2025-06-02 07:33:35 +0530

    Tower of Hanoi Solution

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -4,5 +4,18 @@
 // order of size from top to bottom (i.e.,each disk sits on top of an even larger one
 
 public class TowerOfHanoi {
+    public static void towerOfHanoi(int n, String src, String helper, String dest) {
+        if (n == 1) {
+            System.out.println("transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("transfer disk " + n + " from " + src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
 
+    public static void main(String[] args) {
+        int n = 4;
+        towerOfHanoi(n, "S", "H", "D");
+    }
 }
