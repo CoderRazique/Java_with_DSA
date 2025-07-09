004162fffab96f7e4f0ead96e9237806cbebd732
CoderRazique <abdrazique22@gmail.com>
2025-07-09 23:47:32 +0530

    Tree preorder traversals

---
+++ Trees/TraversalsOfTree.java
@@ -0,0 +1,70 @@
+// Tree preorder traversals 
+
+public class TraversalsOfTree {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+
+        static int idx = -1;
+
+        Node buildTree(int[] nodes) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        void preorder(Node root) { // O(n)
+            if (root == null) {
+                // System.out.print("null ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        void inorder(Node root) { // O(n)
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        void postorder(Node root) { // O(n)
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+        bt.preorder(root);
+        System.out.println();
+        bt.inorder(root);
+        System.out.println();
+        bt.postorder(root);
+    }
+}
