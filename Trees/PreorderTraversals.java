1fc71e44412dafd2e1d7f98ac8d46f42b97f54e8
CoderRazique <abdrazique22@gmail.com>
2025-07-09 23:21:30 +0530

    Tree preorder traversals update

--- Trees/PreorderTraversals.java
+++ Trees/PreorderTraversals.java
@@ -27,12 +27,22 @@ public class PreorderTraversals {
             newNode.right = buildTree(nodes);
             return newNode;
         }
+
+        void preorder(Node root) {
+            if (root == null) {
+                // System.out.print("null ");
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
 
     public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 3, -1, 6, -1, -1 };
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BinaryTree bt = new BinaryTree();
         Node root = bt.buildTree(nodes);
-        System.out.println(root.data);
+        bt.preorder(root);
     }
 }
