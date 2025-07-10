3e17678598af2c35134eda00c17f39000c27aaad
CoderRazique <abdrazique22@gmail.com>
2025-07-10 17:00:44 +0530

    Subtree of another Tree

---
+++ Trees/IsSubtreeOfATree.java
@@ -0,0 +1,72 @@
+// Subtree of another Tree
+// Given the roots of two binary trees root and subRoot,
+// return true if there is a subtree of root with the same structure
+// and node values of subRoot and false otherwise.
+
+//      Tree
+//        1
+//      /   \              subTree
+//     2     3                2
+//    / \   / \              / \
+//   4   5 6   7            4   5
+
+public class IsSubtreeOfATree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+        if (!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public static void main(String[] args) {
+        // Tree
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // subtree
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+        System.out.println(isSubtree(root, subRoot));
+    }
+}
