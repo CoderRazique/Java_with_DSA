d86d7f01ec478619012275802e85006a5ca7c54f
CoderRazique <abdrazique22@gmail.com>
2025-07-10 12:34:29 +0530

    Tree preorder traversals update

--- Trees/TraversalsOfTree.java
+++ Trees/TraversalsOfTree.java
@@ -1,5 +1,8 @@
 // Tree preorder traversals 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TraversalsOfTree {
     public static class Node {
         int data;
@@ -55,6 +58,38 @@ public class TraversalsOfTree {
             postorder(root.right);
             System.out.print(root.data + " ");
         }
+
+        // Level Order Traversal
+
+        void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node currNode = q.remove();
+                if (currNode == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } else {
+                        q.add(null);
+                    }
+                } else {
+                    System.out.print(currNode.data + " ");
+                    if (currNode.left != null) {
+                        q.add(currNode.left);
+                    }
+                    if (currNode.right != null) {
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -66,5 +101,7 @@ public class TraversalsOfTree {
         bt.inorder(root);
         System.out.println();
         bt.postorder(root);
+        System.out.println();
+        bt.levelOrder(root);
     }
 }
