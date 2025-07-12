6ecaf5695a729318dcaa44026e15039537d37667
CoderRazique <abdrazique22@gmail.com>
2025-07-12 23:47:52 +0530

    Print in Range

---
+++ Trees/PrintRange.java
@@ -0,0 +1,75 @@
+// Print in Range
+
+//  k1 =5 , k2 = 12
+
+public class PrintRange {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            root = new Node(data);
+            return root;
+        }
+
+        if (root.data > data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data <= k1) {
+
+            printInRange(root.right, k1, k2);
+
+        } else {
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
+        Node root = null;
+        int k1 = 9, k2 = 14;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
+        printInRange(root, k1, k2);
+
+    }
+}
