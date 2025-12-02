bd181bc06f34f4b45bd2bce71e31ec5b27dc682f
CoderRazique <abdrazique22@gmail.com>
2025-12-03 00:22:26 +0530

    solve 0-1 knapseck problem from tabulation and memoization

--- DP/KnapsackDP.java
+++ DP/KnapsackDP.java
@@ -49,6 +49,36 @@ public class KnapsackDP {
 
     }
 
+    // DP (Tabulation)
+    // O(n * W)
+    public static int knapsackTab(int[] val, int[] wt, int W) {
+        int n = val.length;
+        int[][] DP = new int[n + 1][W + 1];
+        for (int i = 0; i < DP.length; i++) {
+            DP[i][0] = 0;
+        }
+
+        for (int j = 0; j < DP[0].length; j++) {
+            DP[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1]; // ith item val
+                int w = wt[i - 1]; // ith item wt
+                if (w <= j) { // Valid
+                    int incProfit = v + DP[i - 1][j - w];
+                    int excProfit = DP[i - 1][j];
+                    DP[i][j] = Math.max(incProfit, excProfit);
+                } else { // invalid
+                    int excProfit = DP[i - 1][j];
+                    DP[i][j] = excProfit;
+                }
+            }
+        }
+        return DP[n][W];
+    }
+
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
@@ -59,7 +89,8 @@ public class KnapsackDP {
                 DP[i][j] = -1;
             }
         }
-        System.out.println(knapsack(val, wt, W, val.length));
-        System.out.println(knapsackMemoization(val, wt, W, val.length, DP));
+        // System.out.println(knapsack(val, wt, W, val.length));
+        // System.out.println(knapsackMemoization(val, wt, W, val.length, DP));
+        System.out.println(knapsackTab(val, wt, W));
     }
 }
