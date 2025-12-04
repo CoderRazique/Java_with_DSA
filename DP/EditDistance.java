77c871222c01fe6b1014934fa349e277b2e2d869
CoderRazique <abdrazique22@gmail.com>
2025-12-05 01:04:16 +0530

    Edit Distance solution

---
+++ DP/EditDistance.java
@@ -0,0 +1,53 @@
+package DP;
+
+// Edit Distance
+// Given two string word1 and word2, return the minimum number of operations required to convert word1 to word2.
+// YOU have the following three operations permitted on a word:
+//  . Insert a character
+//  . Delete a character
+//  . Replace a character
+
+//  Word1 = "intention"
+//  Word2 = "execution"
+//  Ans = 5 
+
+public class EditDistance {
+
+    // tabulation {O(n*m)}
+    public static int editDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+        // initilize
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = j;
+        }
+
+        // bottom up
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = dp[i][j - 1] + 1;
+                    int delete = dp[i - 1][j] + 1;
+                    int replace = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.min(replace, Math.min(insert, delete));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+        System.out.println(editDistance(word1, word2));
+    }
+
+}
